20c05e5e12922783acaa2a28290d7c99d93e303f
Veikko Ruohonen <veikkoruohonen47@gmail.com>
2026-02-23 09:49:31 +0200

    First Python exercise

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
-print(f"Hei Veikko Ruohonen!")
-nimi = "Alex"
-print(f"Hei {nimi}!")
+ALAMITTA = 37
+
+pituus = float(input("Anna kuhan pituus (cm): "))
+
+if pituus < ALAMITTA:
+    puuttuu = ALAMITTA - pituus
+    print(f"Kuha on alamittainen. Laske se takaisin järveen.")
+    print(f"Alimmasta sallitusta pyyntimitasta puuttuu {puuttuu:.1f} cm.")
+else:
+    print("Kuha on sallitun mittainen.")
--- script1.py
+++
@@ -1,2 +0,0 @@
-nimi = ("Veikko Ruohonen")
-print(f"Hei {nimi}!")
